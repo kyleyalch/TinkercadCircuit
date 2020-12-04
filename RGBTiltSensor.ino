5b12d3aa7343fb4a3c6f3e8acdd1bdb9ba278023
kyleyalch <73710716+kyleyalch@users.noreply.github.com>
2020-12-04 12:26:01 -0500

    Update RGBTiltSensor.ino

--- RGBTiltSensor.ino
+++ RGBTiltSensor.ino
@@ -1,8 +1,9 @@
-int r = 11;
-int g = 10;
-int b = 9;
+int r = 11; // red pin number
+int g = 10; // green pin number
+int b = 9;  // blue pin number
+int tilt = 2;   // tilt sensor input pin number
 
-void setup()
+void setup()   //sets the color signal outputs
 {
   pinMode(11, OUTPUT);
   pinMode(10, OUTPUT);
@@ -11,5 +12,15 @@ void setup()
 
 void loop()
 {
+  int reading;
+  reading = digitalRead(tilt);  // checks the status of the sensor
   
+  if(reading){   // if sensor is stable make the light green
+    digitalWrite(r, LOW);
+    digitalWrite(g, HIGH);
+  }
+  else {        // if sensor is unstable make the light red
+    digitalWrite(r, HIGH);
+    digitalWrite(g, LOW);
+  }
 }
